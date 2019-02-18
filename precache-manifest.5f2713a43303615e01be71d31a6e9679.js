2f0ceb3dce63a8df516feeb4fa0e33f797e3d715
Wayne <wayne.mbanner@gmail.com>
2019-02-18 18:23:57 +0000

    Initial Upload

---
+++ precache-manifest.5f2713a43303615e01be71d31a6e9679.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b750cbfdbed4dfa817932c86ed0c9aa7",
+    "url": "/static/media/space age.b750cbfd.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a94a6c4c2e074f8ef4ca",
+    "url": "/static/js/main.a94a6c4c.chunk.js"
+  },
+  {
+    "revision": "4ae29087961cb4380ddc",
+    "url": "/static/js/2.4ae29087.chunk.js"
+  },
+  {
+    "revision": "a94a6c4c2e074f8ef4ca",
+    "url": "/static/css/main.723eb8b4.chunk.css"
+  },
+  {
+    "revision": "4ae29087961cb4380ddc",
+    "url": "/static/css/2.4d3d3924.chunk.css"
+  },
+  {
+    "revision": "9860aab535cc7106d8cc74eed793b21f",
+    "url": "/index.html"
+  }
+];
